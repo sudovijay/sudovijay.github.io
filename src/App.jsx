662b5b1565adb0c70631b062071fb78e04e5a49f
Vijay Singh <sudovijay@gmail.com>
2018-06-11 12:35:57 +0530

    All set

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import styled from "styled-components";
+
+const Heading = styled.h2`
+    color: red;
+    background: grey;
+    display: flex;
+    text-transform: uppercase;
+`;
 
 class App extends Component {
     render() {
         return (
             <div className="border">
-                <h2>Hello World</h2>
+                <Heading>Hello World</Heading>
             </div>
         );
     }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
--- src/init.js
+++ src/init.js
@@ -1,2 +1,2 @@
 // require js app
-require('./App.jsx');
+import "./App";
