35e6afc1d78e698506a2407cff77412340624a7f
Vijay Singh <sudovijay@gmail.com>
2018-08-06 19:52:26 +0530

    lets run it at 20:00

--- crons/boot.php
+++ crons/boot.php
@@ -16,7 +16,7 @@ $cronObj = new Croner();
  */
 
 // cleaner cron runs every morning 2:30
-$cronObj->register('Cleaner', '19:50');
+$cronObj->register('Cleaner', '20:00');
 
 // scaleway status checker runs every 30 minutes
 $cronObj->register('Scaleway', '00:30');
