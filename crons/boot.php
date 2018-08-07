48e48085b134df1af42f2af45b51c2ceb082f1be
Vijay Singh <sudovijay@gmail.com>
2018-08-07 17:48:33 +0530

    run every 20 minutes

--- crons/boot.php
+++ crons/boot.php
@@ -18,8 +18,8 @@ $cronObj = new Croner();
 // cleaner cron runs every morning 2:30
 $cronObj->register('Cleaner', '2:30');
 
-// scaleway status checker runs every 30 minutes
-$cronObj->register('Scaleway', '00:30');
+// scaleway status checker runs every 20 minutes
+$cronObj->register('Scaleway', '00:20');
 
 /**
  * Now that all registered
