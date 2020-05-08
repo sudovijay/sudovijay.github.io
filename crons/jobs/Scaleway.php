61e00d3c7121ef5c5a7a4a831941f5a882f0c245
Vijay Singh <sudovijay@gmail.com>
2020-05-08 21:35:19 +0530

    added new servers man

--- crons/jobs/Scaleway.php
+++ crons/jobs/Scaleway.php
@@ -10,7 +10,7 @@ class Scaleway
     private $token = '991ad7a9-1d1f-42f5-8a5e-3858a7257039';
     private $user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36';
 
-    function __construct()
+    public function __construct()
     {
         $this->server_array = $this->getServerList();
 
@@ -21,7 +21,7 @@ class Scaleway
         }
     }
 
-    function validateStatus()
+    public function validateStatus()
     {
         foreach ($this->server_array as $server => $key_arry) {
             $http_code = $this->getStatus($server);
@@ -32,7 +32,7 @@ class Scaleway
         }
     }
 
-    function rebootServers()
+    public function rebootServers()
     {
         foreach ($this->reboot_queue as $server => $key_arry) {
             $action_url = str_replace('cp-1', "cp-{$key_arry[1]}1", $this->base_url) . '/servers/' . $key_arry[0] . '/action';
@@ -41,14 +41,14 @@ class Scaleway
         }
     }
 
-    function rebootRequest($action_url)
+    public function rebootRequest($action_url)
     {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $action_url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $headers = array(
             'Content-type: application/json',
-            'X-Auth-Token: ' . $this->token
+            'X-Auth-Token: ' . $this->token,
         );
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($curl, CURLOPT_POST, 1);
@@ -61,7 +61,7 @@ class Scaleway
         $output = curl_exec($curl);
     }
 
-    function getStatus($server)
+    public function getStatus($server)
     {
         $server = 'http://' . $server;
 
@@ -83,12 +83,12 @@ class Scaleway
         return $http_code;
     }
 
-    function getServerList()
+    public function getServerList()
     {
         return [
 
             // ams
-           // '51.15.94.253' => ['dd1066f7-52a8-4f51-9971-a12dce2d3337', 'ams'],
+            // '51.15.94.253' => ['dd1066f7-52a8-4f51-9971-a12dce2d3337', 'ams'],
 
             // paris
             '51.15.237.109' => ['fbe29663-26ad-4e17-a425-a65b7046a040', 'par'],
@@ -97,6 +97,8 @@ class Scaleway
             '51.15.143.134' => ['e6397ad3-c996-45f9-96ae-d2cd45aac797', 'par'],
             '51.15.236.154' => ['6aa5dae4-892a-4540-96b2-0a38a9f7b619', 'par'],
             '51.15.136.68' => ['baca4359-d58f-474d-9ad2-0dcc5bbb0bc8', 'par'],
+            '51.15.211.106' => ['f2eb7e65-98e5-4bc9-bdab-95093fa9f0f8', 'par'],
+            '51.158.126.133' => ['53b06149-3077-42d4-8485-5f04c6c9a036', 'par'],
 
         ];
     }
