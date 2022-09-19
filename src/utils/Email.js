1f734b41ad2eb435122fa9c21bdca56a23e0705a
Vijay Singh <sudovijay@gmail.com>
2022-09-19 05:59:27 +0530

    changes in email

--- src/utils/Email.js
+++ src/utils/Email.js
@@ -1,4 +1,1 @@
-export default () =>
-    [109, 101, 64, 115, 117, 100, 111, 118, 105, 106, 97, 121, 46, 99, 111, 109]
-        .map(el => String.fromCharCode(el).trim())
-        .join("");
+export default () => 'moc.odus321@olleh'.split('').reverse().join(''); // temp
